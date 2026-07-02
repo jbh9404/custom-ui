3e948fca208e88a75006404b69a576f567095855
DESKTOP-AFVHR32\NoteBook <jbh9404@hotmail.com>
2026-07-03 00:15:12 +0900

    code refactoring

--- src/libs/hooks/useToast.ts
+++ src/libs/hooks/useToast.ts
@@ -1,24 +1,31 @@
-import create from "zustand";
+import { create } from "zustand";
 import { ReactNode } from "react";
 
 interface ToastState {
   showToast: boolean;
   toastContent: ReactNode | null;
+  timeoutId: ReturnType<typeof setTimeout> | null;
   handleToast: (toastContent: ReactNode, timer?: number) => void;
 }
 
-const useToast = create<ToastState>((set) => ({
+export const useToast = create<ToastState>((set, get) => ({
   showToast: false,
   toastContent: null,
+  timeoutId: null,
   handleToast: (toastContent, timer = 1700) => {
+    const currentTimeout = get().timeoutId;
+    if (currentTimeout) {
+      clearTimeout(currentTimeout);
+    }
+
+    const newTimeoutId = setTimeout(() => {
+      set({ showToast: false, timeoutId: null });
+    }, timer);
+
     set({
       showToast: true,
       toastContent: toastContent || null,
+      timeoutId: newTimeoutId,
     });
-    setTimeout(() => {
-      set({ showToast: false });
-    }, timer);
   },
 }));
-
-export { useToast };
